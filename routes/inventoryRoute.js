2ba2bd4ce5146ee07f29fdc7dfd43d4d51b1878a
vogurt99 <vogurtsjrg@gmail.com>
2025-11-14 02:15:17 +0800

    Added Friendly URLs to inventoryRoute.js.

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -13,4 +13,13 @@ router.get("/type/:classificationId", invController.buildByClassificationId)
  **************************************** */
 router.get("/detail/:invId", invController.buildByInvId)
 
+/* ****************************************
+ * Friendly URLs for classifications
+ **************************************** */
+router.get("/custom", (req, res) => res.redirect("/inv/type/1"))
+router.get("/sport", (req, res) => res.redirect("/inv/type/2"))
+router.get("/suv", (req, res) => res.redirect("/inv/type/3"))
+router.get("/truck", (req, res) => res.redirect("/inv/type/4"))
+router.get("/sedan", (req, res) => res.redirect("/inv/type/5"))
+
 module.exports = router
